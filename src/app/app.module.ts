fa04c1583e22e2f83160cccaf7197939d7bda37e
Subhashis Suara <subhashis.suara999@gmail.com>
2020-05-19 21:13:49 +0530

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -50,7 +51,8 @@ import { baseURL } from './shared/baseurl';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
